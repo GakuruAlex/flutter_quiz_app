7537b5069c5357416c1b19091702ef4dbbc0f394
GakuruAlex <gakurualexgicimo@gmail.com>
2025-12-25 19:01:55 +0300

    Use state from Quiz Widget to change screen to quiz questions

---
+++ lib/home_ui.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/quiz_button.dart';
+
+class HomeUI extends StatelessWidget {
+  const HomeUI(this.quizScreen, {super.key});
+  final void Function() quizScreen;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      spacing: 30.0,
+
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          width: 300,
+          color: Color.fromARGB(187, 250, 249, 248),
+        ),
+
+        Text(
+          'Learn Flutter The Fun Way!',
+          style: TextStyle(fontSize: 18, color: Colors.white),
+        ),
+
+        QuizButton(contentText: 'Start Quiz', switchScreen: quizScreen),
+      ],
+    );
+  }
+}
