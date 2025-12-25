5fb6a486e7642285b942f3b6b899732268912376
GakuruAlex <gakurualexgicimo@gmail.com>
2025-12-25 19:05:44 +0300

    Create a stateful widget to handle state for changing screens from Home Ui to Quiz Questions UI

---
+++ lib/quiz.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/home_ui.dart';
+import 'package:flutter_quiz_app/questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? currentScreen;
+
+  @override
+  void initState() {
+    currentScreen = HomeUI(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      currentScreen = const QuestionScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: AlignmentGeometry.topLeft,
+              end: AlignmentGeometry.bottomRight,
+            ),
+          ),
+          child: Center(child: currentScreen),
+        ),
+      ),
+    );
+  }
+}
