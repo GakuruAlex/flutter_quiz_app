0cde5becb07a73a8ead0b7d553a10753be16cb21
GakuruAlex <gakurualexgicimo@gmail.com>
2025-12-26 12:52:03 +0300

    Use an AnswerButton widget to display all the answers for given question

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/answer_button.dart';
+import 'package:flutter_quiz_app/data/questions.dart';
+import 'package:flutter_quiz_app/models/quiz_question.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -7,8 +10,27 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  var currentQuestion = questions[1];
   @override
   Widget build(BuildContext context) {
-    return Text('Question Screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.questionText,
+            style: TextStyle(
+              color: Color.fromARGB(255, 255, 254, 240),
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(answers: currentQuestion.answers),
+        ],
+      ),
+    );
   }
 }
