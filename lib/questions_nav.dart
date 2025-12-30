76519476feca0935b9d0691142c9da15e92e8754
GakuruAlex <gakurualexgicimo@gmail.com>
2025-12-30 16:33:37 +0300

    Create a nav widget that returns a row of buttons for next and previous, based on if there are more questions

---
+++ lib/questions_nav.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/data/questions.dart';
+import 'package:flutter_quiz_app/models/quiz_question.dart';
+import 'package:flutter_quiz_app/quiz_button.dart';
+
+class QuestionNav extends StatelessWidget {
+  const QuestionNav({
+    super.key,
+    required int currentQuesitionIndex,
+    required this.previous,
+    required this.next,
+    required this.isPressed,
+  }) : _currentQuesitionIndex = currentQuesitionIndex;
+
+  final int _currentQuesitionIndex;
+  final IconData previous;
+  final IconData next;
+  final Function isPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        if (_currentQuesitionIndex > 0 &&
+            _currentQuesitionIndex <= questions.length)
+          QuizButton(
+            contentText: 'Previous',
+            switchScreen: () {
+              isPressed('prev');
+            },
+            chosenIcon: previous,
+          ),
+        if (_currentQuesitionIndex >= 0 &&
+            _currentQuesitionIndex < questions.length - 1)
+          QuizButton(
+            contentText: 'Next',
+            switchScreen: () {
+              isPressed('next');
+            },
+            chosenIcon: next,
+          ),
+      ],
+    );
+  }
+}
