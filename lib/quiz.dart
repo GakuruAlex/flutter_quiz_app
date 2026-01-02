f4f7ff8406e9662647547d433537975b8ea0b63b
GakuruAlex <gakurualexgicimo@gmail.com>
2026-01-02 23:52:48 +0300

    Lift switchScreen to Quize widget , to switch between homepage, questions screen and results. and also move from one questions to the next

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz_app/data/questions.dart';
 import 'package:flutter_quiz_app/home_ui.dart';
+import 'package:flutter_quiz_app/models/quiz_question.dart';
 import 'package:flutter_quiz_app/questions_screen.dart';
 import 'package:flutter_quiz_app/results_screen.dart';
 
@@ -16,11 +17,28 @@ class _QuizState extends State<Quiz> {
   Map<int, List<String>> shuffledAnswers = {};
 
   List<String> selectedAnswers = List<String>.filled(questions.length, '');
+  int _currentQuestionIndex = 0;
+  // ignore: unused_field
+  QuizQuestion _currentQuestion = questions[0];
 
-  void switchScreen(String screen) {
-    setState(() {
-      currentScreen = screen;
-    });
+  @override
+  void initState() {
+    super.initState();
+
+    _currentQuestion = questions[_currentQuestionIndex];
+  }
+
+  void switchScreen(String page) {
+    if (page == 'next' || page == 'prev') {
+      page == 'next' ? _currentQuestionIndex += 1 : _currentQuestionIndex -= 1;
+      setState(() {
+        _currentQuestion = questions[_currentQuestionIndex];
+      });
+    } else {
+      setState(() {
+        currentScreen = page;
+      });
+    }
   }
 
   @override
@@ -41,11 +59,13 @@ class _QuizState extends State<Quiz> {
           child: Center(
             child: currentScreen == 'home-screen'
                 ? HomeUI(switchScreen: switchScreen)
-                : currentScreen == 'questions_screen'
+                : currentScreen == 'questions-screen'
                 ? QuestionScreen(
                     switchScreen: switchScreen,
                     shuffledAnswers: shuffledAnswers,
-                    selectedAnswer: selectedAnswers,
+                    selectedAnswers: selectedAnswers,
+                    currentQuestionIndex: _currentQuestionIndex,
+                    currentQuestion: _currentQuestion,
                   )
                 : ResultsScreen(),
           ),
