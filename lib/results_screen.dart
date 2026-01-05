88a92d059120b891ffe3a4f2538f122d17c7e482
GakuruAlex <gakurualexgicimo@gmail.com>
2026-01-05 21:14:41 +0300

    Show the question number clor depending on whether the answer is correct or not

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -3,53 +3,67 @@ import 'package:flutter_quiz_app/data/questions.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key, required this.selectedAnswers});
+  ResultsScreen({super.key, required this.selectedAnswers});
 
   final List<String> selectedAnswers;
 
+  final TextStyle questionsFont = GoogleFonts.notoSansDisplay(
+    color: Color.fromARGB(255, 255, 255, 255),
+    fontSize: 18,
+  );
+
+  final TextStyle userAnswersFont = GoogleFonts.notoSansDisplay(
+    color: Color.fromARGB(255, 217, 6, 245),
+    fontSize: 14,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 78, 13, 151),
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 107, 15, 168),
-        title: Text(
-          'Flutter Quiz Results',
-          style: GoogleFonts.notoSansDisplay(
-            color: Color.fromARGB(255, 255, 255, 255),
-          ),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 78, 13, 151),
+            Color.fromARGB(255, 107, 15, 168),
+          ],
         ),
       ),
-      body: Scrollbar(
-        scrollbarOrientation: ScrollbarOrientation.right,
-        controller: ScrollController(initialScrollOffset: 0.0),
-        child: SizedBox(
-          width: 375,
-          height: 500,
-          child: ListView.separated(
-            itemBuilder: (BuildContext context, int index) {
-              return Column(
-                children: [
-                  QuestionText(
-                    selectedAnswers: selectedAnswers,
-                    index: index,
-                  ),
 
-                  ResultText(
-                    selectedAnswers: selectedAnswers,
-                    index: index,
+      child: Center(
+        child: Scrollbar(
+          scrollbarOrientation: ScrollbarOrientation.right,
+          controller: ScrollController(initialScrollOffset: 0.0),
+          child: SizedBox(
+            width: 375,
+            height: 500,
+            child: ListView.separated(
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  children: [
+                    QuestionText(
+                      selectedAnswers: selectedAnswers,
+                      index: index,
+                      questionsFont: questionsFont,
+                    ),
+
+                    ResultText(
+                      selectedAnswers: selectedAnswers,
+                      index: index,
+                      userAnswersFont: userAnswersFont,
+                    ),
+                    CorrectAnswer(
+                      index: index,
+                      userAnswersFont: userAnswersFont,
+                    ),
+                  ],
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const SizedBox(
+                    height: 30.0,
                   ),
-                  CorrectAnswer(
-                    index: index,
-                  ),
-                ],
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) =>
-                const Divider(
-                  height: 30.0,
-                ),
-            itemCount: questions.length,
+              itemCount: questions.length,
+            ),
           ),
         ),
       ),
@@ -58,8 +72,14 @@ class ResultsScreen extends StatelessWidget {
 }
 
 class CorrectAnswer extends StatelessWidget {
-  const CorrectAnswer({super.key, required this.index});
+  const CorrectAnswer({
+    super.key,
+    required this.index,
+    required this.userAnswersFont,
+  });
   final int index;
+
+  final TextStyle userAnswersFont;
 
   @override
   Widget build(BuildContext context) {
@@ -67,10 +87,7 @@ class CorrectAnswer extends StatelessWidget {
       children: [
         Text(
           questions[index].answers[0],
-          style: TextStyle(
-            fontSize: 22,
-            color: Color.fromARGB(255, 255, 255, 255),
-          ),
+          style: userAnswersFont,
         ),
       ],
     );
@@ -82,31 +99,32 @@ class QuestionText extends StatelessWidget {
     super.key,
     required this.index,
     required this.selectedAnswers,
+    required this.questionsFont,
   });
   final int index;
   final List<String> selectedAnswers;
+
+  final TextStyle questionsFont;
   @override
   Widget build(BuildContext context) {
     return Row(
+      spacing: 10,
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         // Question number widget
         Container(
+          padding: EdgeInsets.all(6),
           decoration: BoxDecoration(
             shape: BoxShape.circle,
             color: questions[index].answers[0] == selectedAnswers[index]
-                ? Color.fromARGB(255, 107, 247, 88)
+                ? Color.fromARGB(255, 153, 241, 142)
                 : Color.fromRGBO(247, 149, 149, 1),
           ),
-          padding: EdgeInsets.all(4),
 
           child: Text(
-            '${index + 1}.',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 22,
-            ),
+            '${index + 1}',
+            style: questionsFont,
           ),
         ),
 
@@ -114,10 +132,7 @@ class QuestionText extends StatelessWidget {
         Expanded(
           child: Text(
             questions[index].questionText,
-            style: TextStyle(
-              fontSize: 22,
-              color: Color.fromARGB(255, 255, 255, 255),
-            ),
+            style: questionsFont,
           ),
         ),
       ],
@@ -130,22 +145,19 @@ class ResultText extends StatelessWidget {
     super.key,
     required this.selectedAnswers,
     required this.index,
+    required this.userAnswersFont,
   });
 
   final List<String> selectedAnswers;
   final int index;
 
+  final TextStyle userAnswersFont;
+
   @override
   Widget build(BuildContext context) {
     return Wrap(
       children: [
-        Text(
-          selectedAnswers[index],
-          style: TextStyle(
-            fontSize: 22,
-            color: Color.fromARGB(255, 255, 255, 255),
-          ),
-        ),
+        Text(selectedAnswers[index], style: userAnswersFont),
       ],
     );
   }
